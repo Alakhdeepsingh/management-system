fa9b8f90c9f482d6c78851f318f371732318e9fa
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2025-04-29 23:21:07 +0530

    management-system

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Signup.css";
+import "./SignUp.css";
 
 const Signup = () => {
     const [formData, setFormData] = useState({
